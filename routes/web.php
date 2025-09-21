abba06a1b0537bb522893d442d1bb06722f872fe
paguero4300 <paguero@digito.pe>
2025-09-21 16:30:54 -0500

    more changes

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InvoicePdfController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/admin');
 });
 
 // Rutas para PDFs de facturas
